d72fbe5922b5e63489f96a2c4590a5d730a07c2d
Michael Abah <donsmartez@gmail.com>
2021-07-15 13:12:57 +0100

    arranging category page

--- src/pages/categories.js
+++ src/pages/categories.js
@@ -1,6 +1,6 @@
 // import { useQuery } from '@apollo/client'
 import {CategoryList} from '../components/categoryList'
-import {HeroSection} from '../components/hero'
+import {CategoryHeroSection, HeroSection} from '../components/hero'
 // import { CATEGORY_QUERY } from '../query'
 
 export const Category =()=> {
@@ -21,7 +21,7 @@ export const Category =()=> {
                         
                     </div>
                     <div className="col-md-9 my-4 py-2">
-                        <HeroSection bglink="/category banner.jpg" text="Order goods from a variety of online stores" />
+                        <CategoryHeroSection bglink="/category banner.jpg" text="Order goods from a variety of online stores" />
                         <h2 className="py-4">Product type</h2>
                         <p>At mattis quis mi in hac proin metus velit. Id consectetur odio magnis sollicitudin purus mauris. Adipiscing purus sodales velit sem. Purus lacus faucibus feugiat diam. At in ultrices egestas sollicitudin ipsum. Sollicitudin ipsum duis faucibus lobortis malesuada. Potenti sagittis at egestas quis nunc viverra orci, volutpat. </p>
                         <h2 className="py-4">Uk Store</h2>
@@ -38,44 +38,10 @@ export const Category =()=> {
                             <div className="col-md-4 my-2">
                                 <img src="/Image and Icon.png" className="w-100" />
                             </div>
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
-                        </div>
-                        <h2 className="py-4">Uk Store</h2>
-                        <div className="row">
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
-                            <div className="col-md-4 my-2">
-                                <img src="/Image and Icon.png" className="w-100" />
-                            </div>
+                            
                             
                         </div>
+                        
                     </div>
                 </div>
             </section>
